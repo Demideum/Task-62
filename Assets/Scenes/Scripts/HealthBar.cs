f6237bc7e97afa869f395501738a31285f575371
Demideum <102413714+Demideum@users.noreply.github.com>
2022-09-22 19:44:34 +0500

    Task-62
    First version

---
+++ Assets/Scenes/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Image _imageFillHealthBar;
+    [SerializeField] private Player _player;
+
+    private void OnEnable()
+    {
+        _player.ChangingHealth += OnChangingHealth;
+    }
+
+    private void OnDisable()
+    {
+        _player.ChangingHealth -= OnChangingHealth;
+    }
+
+    private void OnChangingHealth()
+    {
+        float target = _player.Health / _player.MaxHealth;
+        _imageFillHealthBar.DOFillAmount(target, _imageFillHealthBar.fillAmount);
+    }
+}
